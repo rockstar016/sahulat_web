9336579424ca2e507fe314d240ecae538ee30fb6
aasongWang <zhongokhyon93@yahoo.com>
2017-04-12 04:32:50 +0800

    Update Dashboard.php

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -18,40 +18,33 @@ class Dashboard extends CI_Controller {
             $left_view = $this->load->view('template/leftview',$data_left, true);
 
             $data['leftview'] = $left_view;
-            $user_list = $this->client_model->getAllClient("");
+            $user_list = $this->dashboard_model->getAllClient("");
             $data['userlist'] = $user_list;
             $data['search'] = "";
-            $this->load->view('admin/customers', $data);
+            $this->load->view('admin/dashboard', $data);
         }
     }
 
-    public function tableone()
+    public function GetIntegratedData()
     {
-        if($this->session->has_userdata('logged_in') == false){
-            redirect('admin/');
-        }
-        else{
-            $admin = $this->session->userdata['master'];
-            $data_header['admin'] = $admin;
-            $data_header['page'] = 'customers';
-            $this->load->view('template/headerview', $data_header);
+        $tabledata1 = $this->GetJsonData();
 
-            $data_left['category'] = 0;	//Category manage page selected.
-            $data_left['sub_category'] = 0;	//Sub Category manage page selected.
-            $left_view = $this->load->view('template/leftview',$data_left, true);
+        $tabledata2 = $this->GetJsonDataTableTwo();
 
-            $data['leftview'] = $left_view;
-            $user_list = $this->client_model->getAllClient("");
-            $data['userlist'] = $user_list;
-            $data['search'] = "";
-            $this->load->view('admin/dashboard_tableone', $data);
-        }
+        $tabledata3 = $this->GetJsonDataTableThree();
+
+        $tabledata5 = $this->GetJsonDataTableFive();
+
+        $table1 = array("tbData1"=>$tabledata1,"tbData2"=>$tabledata2, "tbData3"=>$tabledata3, "tbData4"=>"","tbData5" => $tabledata5);
+
+        $pp = json_encode($table1);
+        echo $pp;
     }
 
     public function GetJsonData()
     {
 
-        $pending_array = $this->order_model->getOrdersForTableOne();
+        $pending_array = $this->dashboard_model->getOrdersForTable();
         $totalData = count($pending_array);
 
         $data = array();
@@ -67,16 +60,16 @@ class Dashboard extends CI_Controller {
                     break;
                 case 1:
                     $status = "Accepted";
-                     break;
+                    break;
                 case 2:
                     $status = "Completed";
                     break;
             }
 
-            $client_data = $this->client_model->get_client($client_id);
+            $client_data = $this->dashboard_model->get_client($client_id);
             $pending_array[$i]['client'] = $client_data;
             $service_id = $pending_array[$i]['service_id'];
-            $service_data = $this->client_model->get_client($service_id);
+            $service_data = $this->dashboard_model->get_client($service_id);
             $pending_array[$i]['service'] = $service_data;
 
             $no = $i+1;
@@ -89,53 +82,24 @@ class Dashboard extends CI_Controller {
             }
 
             $createdAt = $client_data[0]['created_at'];
-            $updateat = $client_data[0]['updated_at'];
-            $orderdata =  $pending_array[$i]['order_date'];
-
-            $linkid = $pending_array[$i]['id'];
-            $nestedData = array();
-
             $row_data = array('no' => $no,"created_at" => $createdAt,"State" =>$status, "username" => $username, "servicename" => $servicename);
             array_push($data,$row_data);
 
         }
 
-        $result = json_encode($data);
-        echo $result;
+        return  $data;
 
     }
-    
-    public function tabletwo()
-    {
-        if($this->session->has_userdata('logged_in') == false){
-            redirect('admin/');
-        }
-        else{
-            $admin = $this->session->userdata['master'];
-            $data_header['admin'] = $admin;
-            $data_header['page'] = 'customers';
-            $this->load->view('template/headerview', $data_header);
 
-            $data_left['category'] = 0;	//Category manage page selected.
-            $data_left['sub_category'] = 0;	//Sub Category manage page selected.
-            $left_view = $this->load->view('template/leftview',$data_left, true);
 
-            $data['leftview'] = $left_view;
-            $user_list = $this->client_model->getAllClient("");
-            $data['userlist'] = $user_list;
-            $data['search'] = "";
-            $this->load->view('admin/dashboard_tabletwo', $data);
-        }
-    }
-
-     public function GetJsonDataTableTwo()
+    public function GetJsonDataTableTwo()
     {
 
         $date = date("Y-m-d");
         $prevdate = date("Y-m-d", mktime(0, 0, 0, date("m"),date("d")-1,date("Y")));
 
 
-        $model_array = $this->order_model->getOrdersForTableOne();
+        $model_array = $this->dashboard_model->getOrdersForTable();
         $totalData = count($model_array);
 
         $data = array();
@@ -149,7 +113,7 @@ class Dashboard extends CI_Controller {
             if((strpos($tmpDate,$date) >0))
             {
                 $updatedate = "Today";
-             }
+            }
             else if(strpos($tmpDate,$prevdate) >0)
             {
                 $updatedate = "Yesterday";
@@ -159,7 +123,7 @@ class Dashboard extends CI_Controller {
                 continue;
             }
 
-             $client_id = $model_array[$i]['client_id'];
+            $client_id = $model_array[$i]['client_id'];
             switch ($model_array[$i]['status'])
             {
                 case 0:
@@ -173,10 +137,10 @@ class Dashboard extends CI_Controller {
                     break;
             }
 
-            $client_data = $this->client_model->get_client($client_id);
+            $client_data = $this->dashboard_model->get_client($client_id);
             $model_array[$i]['client'] = $client_data;
             $service_id = $model_array[$i]['service_id'];
-            $service_data = $this->client_model->get_client($service_id);
+            $service_data = $this->dashboard_model->get_client($service_id);
             $model_array[$i]['service'] = $service_data;
 
             $no = $i+1;
@@ -193,9 +157,97 @@ class Dashboard extends CI_Controller {
 
         }
 
-        $result = json_encode($data);
-        echo $result;
+        return $data;
 
+    }
+
+    public function GetJsonDataTableThree()
+    {
+
+        $model_array = $this->dashboard_model->getOrdersForTable();
+        $totalData = count($model_array);
+
+        $data = array();
+
+        for($i = 0; $i < count($model_array); $i++){
+
+            $tmpDate = $model_array[$i]['updated_at'];
+
+            $client_id = $model_array[$i]['client_id'];
+            $client_data = $this->dashboard_model->get_client($client_id);
+
+            $model_array[$i]['client'] = $client_data;
+            $service_id = $model_array[$i]['service_id'];
+
+            $service_data = $this->dashboard_model->get_client($service_id);
+            $model_array[$i]['service'] = $service_data;
+
+            $no = $i+1;
+            $username = $client_data[0]['user_name'];
+            $servicename = "";
+
+            if(!empty($model_array[$i]['service'][0]['user_name']))
+            {
+                $servicename = ($model_array[$i]['service'][0]['user_name']);
+            }
+            $orderdate =  $model_array[$i]['order_date'];
+            $row_data = array('no' => $no,"username" => $username, "created_date" => $servicename,"order_date" =>$orderdate);
+            array_push($data,$row_data);
+
+        }
+
+        return $data;
+
+    }
+
+    public function GetJsonDataTableFour(){
+        $client_arr = $this->dashboard_model->getAllServiceman("");
+        $data = array();
+
+        for($clId = 0; $clId < count($client_arr); $clId++)
+        {
+            $pp = $client_arr[$clId];
+            $servicename  = $pp['user_name'];
+            $phonenumber =  $client_arr[$clId]['user_phone'];
+            $servicemanID = $clId;
+            $jobAssigned = $this->dashboard_model->getNumberJob($clId,0);
+            $JobProcessedNumber = $this->dashboard_model->getNumberJob($clId,1);
+            $JobCompletedNumber = $this ->dashboard_model ->getNumberJob($clId,2);
+            $JobPendingNumber = 0;
+            $AverrespControl = 0;
+            $Averrespclient = 0;
+            $rating = $this->dashboard_model->getRating($servicemanID);
+            $row_data = array('no' => $clId,"servicename" => $servicename, "phonenum" => $phonenumber,"Job_Assign"=>$jobAssigned,"JobProcess"=>$JobProcessedNumber,"JobPend"=>$JobPendingNumber,"AvgRespControl"=>$AverrespControl,"JobComplete"=>$JobCompletedNumber,"AvgrepClient"=>$Averrespclient,"rating"=>$rating);
+            array_push($data,$row_data);
+        }
+
+        return $data;
+    }
+
+    public function GetJsonDataTableFive(){
+
+        $client_arr = $this->dashboard_model->getAllServiceman("");
+        $data = array();
+
+        for($clId = 0; $clId < count($client_arr); $clId++)
+        {
+            $pp = $client_arr[$clId];
+            $servicename  = $pp['user_name'];
+            $phonenumber =  $client_arr[$clId]['user_phone'];
+            $servicemanID = $clId;
+            $jobAssigned = $this->dashboard_model->getNumberJob($clId,0);
+            $JobProcessedNumber = $this->dashboard_model->getNumberJob($clId,1);
+            $JobCompletedNumber = $this ->dashboard_model ->getNumberJob($clId,2);
+            $JobPendingNumber = 0;
+            $AverrespControl = 0;
+            $Averrespclient = 0;
+            $rating = $this->dashboard_model->getRating($servicemanID);
+            $row_data = array('no' => $clId,"servicename" => $servicename, "phonenum" => $phonenumber,"Job_Assign"=>$jobAssigned,"JobProcess"=>$JobProcessedNumber,"JobPend"=>$JobPendingNumber,"AvgRespControl"=>$AverrespControl,"JobComplete"=>$JobCompletedNumber,"AvgrepClient"=>$Averrespclient,"rating"=>$rating);
+            array_push($data,$row_data);
+
+        }
+
+        return $data;
     }
 
 }
